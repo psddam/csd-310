532f37a949aa0c2efcd9d1e78df375bcc8c358db
psddam <simonpeter2567@gmail.com>
2026-07-04 09:18:16 -0500

    adding module-6

---
+++ module-6/movies_queries.py
@@ -0,0 +1,62 @@
+import mysql.connector
+from mysql.connector import errorcode
+from dotenv import dotenv_values
+
+# Load database login information from .env file
+secrets = dotenv_values(".env")
+
+config = {
+    "user": secrets["USER"],
+    "password": secrets["PASSWORD"],
+    "host": secrets["HOST"],
+    "database": secrets["DATABASE"],
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+
+    print("\n-- DISPLAYING Studio RECORDS --")
+    cursor.execute("SELECT * FROM studio")
+    studios = cursor.fetchall()
+
+    for studio in studios:
+        print(f"Studio ID: {studio[0]}")
+        print(f"Studio Name: {studio[1]}\n")
+
+    print("\n-- DISPLAYING Genre RECORDS --")
+    cursor.execute("SELECT * FROM genre")
+    genres = cursor.fetchall()
+
+    for genre in genres:
+        print(f"Genre ID: {genre[0]}")
+        print(f"Genre Name: {genre[1]}\n")
+
+    print("\n-- DISPLAYING Short Film RECORDS --")
+    cursor.execute("SELECT film_name, film_runtime FROM film WHERE film_runtime < 120")
+    films = cursor.fetchall()
+
+    for film in films:
+        print(f"Film Name: {film[0]}")
+        print(f"Runtime: {film[1]}\n")
+
+    print("\n-- DISPLAYING Director RECORDS in Order --")
+    cursor.execute("SELECT film_name, film_director FROM film ORDER BY film_director")
+    directors = cursor.fetchall()
+
+    for director in directors:
+        print(f"Film Name: {director[0]}")
+        print(f"Director: {director[1]}\n")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password is invalid.")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist.")
+    else:
+        print(err)
+
+finally:
+    if 'db' in locals() and db.is_connected():
+        db.close()
